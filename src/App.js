b39f0ecaaac0b7d7d21edd2c7a1238a42cab6382
Daniel Koller <daniel-koller@gmx.at>
2023-01-18 19:02:32 +0100

    Simplify and fix download function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import download from 'downloadjs';
+import { saveAs } from 'file-saver';
 import React, { useState } from 'react';
 
 function MemeGenerator() {
   const [topText, setTopText] = useState('');
   const [bottomText, setBottomText] = useState('');
-  const [template, setTemplate] = useState('doge');
-  const [memeUrl, setMemeUrl] = useState(
-    `https://memegen.link/${template}/${topText}/${bottomText}.jpg`,
-  );
+  const [template, setTemplate] = useState('fine');
 
   const handleTopTextChange = (event) => {
     setTopText(event.target.value);
@@ -20,16 +17,6 @@ function MemeGenerator() {
   const handleTemplateChange = (event) => {
     setTemplate(event.target.value);
   };
-
-  React.useEffect(() => {
-    setMemeUrl(`https://memegen.link/${template}/${topText}/${bottomText}.jpg`);
-  }, [topText, bottomText, template]);
-
-  // Uses downloadjs to download the generated meme
-  function handleDownload() {
-    const fileName = `${template}-${topText}-${bottomText}.jpg`;
-    download(memeUrl, fileName);
-  }
 
   return (
     <div>
@@ -50,9 +37,23 @@ function MemeGenerator() {
         onChange={handleTemplateChange}
       />
 
-      <img data-test-id="meme-image" src={memeUrl} alt="Meme" />
+      <img
+        style={{ width: '300px' }}
+        src={`https://api.memegen.link/images/${template}/${topText}/${bottomText}`}
+        alt="meme"
+        data-test-id="meme-image"
+      />
 
-      <button onClick={handleDownload}>Download</button>
+      <button
+        onClick={() => {
+          saveAs(
+            `https://api.memegen.link/images/${template}/${topText}/${bottomText}`,
+            `${template}.jpg`,
+          );
+        }}
+      >
+        Download
+      </button>
     </div>
   );
 }
